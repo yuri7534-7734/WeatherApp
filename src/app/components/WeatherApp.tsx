a570d125de06b5690980464088fcfd0a9ab07736
yuri7534 <yuri7534@naver.com>
2026-03-15 22:12:20 +0900

    Update WeatherApp.tsx

--- src/app/components/WeatherApp.tsx
+++ src/app/components/WeatherApp.tsx
@@ -184,6 +184,7 @@ export const WeatherApp: React.FC = () => {
       </div>
 
       <div className={styles.header}>
+        <h1>테스트12345</h1>
         <div className={styles.cityName}>서울특별시임미댜</div>
       </div>
 
